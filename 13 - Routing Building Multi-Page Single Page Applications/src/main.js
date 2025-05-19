7c9a61d02d753c72bb159a78ae92e60cd7ed1d43
tienvuong <tienvm272@thefirstone.jp>
2025-05-19 21:22:18 +0700

    hoc het video 19

--- 13 - Routing Building Multi-Page Single Page Applications/src/main.js
+++ 13 - Routing Building Multi-Page Single Page Applications/src/main.js
@@ -7,18 +7,45 @@ import NotFound from './components/nav/NotFound.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
 import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
+import TeamsFooter from './components/teams/TeamsFooter.vue'
+import UsersFooter from './components/users/UsersFooter.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
     // { path: '/teams', component: TeamsList, alias: '/' },
-    { path: '/teams', component: TeamsList },
-    { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    {
+      name: 'teams',
+      path: '/teams',
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [{ name: 'team-members', path: ':teamId', component: TeamMembers, props: true }],
+    },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
+  scrollBehavior(_, _2, savedPosition) {
+    // console.log(to, from, savedPosition)
+    if (savedPosition) {
+      return savedPosition
+    }
+    return {
+      left: 0,
+      top: 0,
+    }
+  },
+})
+
+router.beforeEach(function (to, from, next) {
+  console.log('Global beforeEach')
+  console.log(to, from)
+  // if (to.name === 'team-members') {
+  //   next()
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } })
+  // }
+  next()
 })
 
 const app = createApp(App)
